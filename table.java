a09f4934723a44df96ae1428d62d5ca2778fdd69
AYUSH22006 <ayushsao2006@gmail.com>
2026-08-20 14:11:18 +0530

    9

---
+++ table.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+           System.out.print("Table of Number:");
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+
+        for ( int i=1 ; i<11 ; i++) {
+            System.out.println(number+"x"+i+"="+i*number);
+        }
+        
+    }
+    
+}
